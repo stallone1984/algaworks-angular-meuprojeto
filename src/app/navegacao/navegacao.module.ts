cd980068b15788b3092fc910a678c8ad2eaa02b9
Heitor Bernardino <heitor.bernardino@S2IT.CORP>
2020-02-19 11:27:28 -0300

    14-4 Criando um componente interno do módulo

--- src/app/navegacao/navegacao.module.ts
+++ src/app/navegacao/navegacao.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { BotoesModule } from '../botoes/botoes.module';
+import { LinhaComponent } from './linha/linha.component';
 
 
 
 @NgModule({
   declarations: [
-    MenuComponent
+    MenuComponent,
+    LinhaComponent
   ],
   imports: [
     CommonModule,
