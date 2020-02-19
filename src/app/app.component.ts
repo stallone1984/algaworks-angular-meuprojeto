ff27e1feda211099321358627e2f9f162d53c312
Heitor Bernardino <heitor.bernardino@S2IT.CORP>
2020-02-19 19:10:17 -0300

    15-2 Implementando um serviço

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FuncionarioService } from './funcionario.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   funcionarios = [];
 
-  aoAdicionar(funcionario) {
-    this.funcionarios.push(funcionario);
+  funcionarioService: FuncionarioService;
+
+  constructor(){
+    this.funcionarioService = new FuncionarioService()
   }
 
+  aoAdicionar(funcionario) {
+    this.funcionarioService.adicionar(funcionario)
+    this.funcionarios = this.funcionarioService.consultar();
+  }
+
+  ngOnInit() {
+    this.funcionarios = this.funcionarioService.consultar();
+  }
 }
---
+++ src/app/funcionario.service.ts
@@ -0,0 +1,15 @@
+export class FuncionarioService {
+
+    ultimoId = 1;
+    funcionarios = [];
+
+    adicionar(funcionario) {
+        this.funcionarios.push(funcionario);
+        console.log(JSON.stringify(this.funcionarios));
+        
+    }
+
+    consultar() {
+        return this.funcionarios;
+    }
+}
